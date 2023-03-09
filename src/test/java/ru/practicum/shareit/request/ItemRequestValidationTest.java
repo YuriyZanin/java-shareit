323c12263dfb9a752c9cf7eafc5319cfe393b9d1
yuriy <y.v.zanin@samsmu.ru>
2023-03-09 18:10:31 +0400

    feat: реализовать тесты для валидации данных запросов

---
+++ src/test/java/ru/practicum/shareit/request/ItemRequestValidationTest.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.request;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.AbstractValidationTest;
+import ru.practicum.shareit.request.dto.ItemRequestCreationDto;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ItemRequestValidationTest extends AbstractValidationTest {
+    @Test
+    void shouldBeSuccessValidation() {
+        ItemRequestCreationDto creationDto = new ItemRequestCreationDto(null, "description");
+
+        Set<ConstraintViolation<ItemRequestCreationDto>> violations = validator.validate(creationDto);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldBeFailedIfDescriptionIsNull() {
+        ItemRequestCreationDto creationDto = new ItemRequestCreationDto(null, null);
+
+        Set<ConstraintViolation<ItemRequestCreationDto>> violations = validator.validate(creationDto);
+        assertEquals(1, violations.size());
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals("description")));
+    }
+
+    @Test
+    void shouldBeFailedIfDescriptionIsEmpty() {
+        ItemRequestCreationDto creationDto = new ItemRequestCreationDto(null, "");
+
+        Set<ConstraintViolation<ItemRequestCreationDto>> violations = validator.validate(creationDto);
+        assertEquals(1, violations.size());
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals("description")));
+    }
+
+    @Test
+    void shouldBeFailedIfDescriptionIsBlank() {
+        ItemRequestCreationDto creationDto = new ItemRequestCreationDto(null, "   ");
+
+        Set<ConstraintViolation<ItemRequestCreationDto>> violations = validator.validate(creationDto);
+        assertEquals(1, violations.size());
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals("description")));
+    }
+}
