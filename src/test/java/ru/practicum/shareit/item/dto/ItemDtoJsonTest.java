759bf3c62e12bdd0b20546d481e0f33e45add32d
Yuriy Zanin <y.v.zanin@yandex.ru>
2023-03-13 20:55:02 +0400

    fix: привести в соотвествие code style

--- src/test/java/ru/practicum/shareit/item/dto/ItemDtoJsonTest.java
+++ src/test/java/ru/practicum/shareit/item/dto/ItemDtoJsonTest.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
-import ru.practicum.shareit.TestData;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static ru.practicum.shareit.TestData.*;
+import static ru.practicum.shareit.TestData.itemDto;
 
 @JsonTest
 public class ItemDtoJsonTest {
